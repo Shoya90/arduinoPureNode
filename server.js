6b73dd6854bdf5940343f949ee3de117d74068f7
Shoya90 <fiansafee@gmail.com>
2017-09-05 23:09:14 +0200

    D O N E

--- server.js
+++ server.js
@@ -4,34 +4,56 @@ var url = require("url");
 
 var port = 8080;
 var obj = {};
-obj.busy = false;
+obj.busy = true;
 
 var httpServer = http.createServer(function(req, res){
 
   switch(req.method){
+
+
+
     case "GET":
     if(req.url === '/data'){
       res.writeHeader(200, {'Content-type' : 'application/json'});
-      res.write(JSON.stringify(obj));
+      res.write(JSON.stringify(obj.busy));
+      res.end();
+    }
+    else if(req.url === '/end'){
+      res.writeHeader(200, {'Content-type' : 'application/json'});
+      obj.busy = false;
+      res.end();
+    }
+    else if(req.url === '/ready'){
+      res.writeHeader(200, {'Content-type' : 'application/json'});
+      obj.busy = false;
       res.end();
     }
     break;
 
     case "POST":
-      var data = url.parse(req.url, true).query;
-      var binary_data="";
-      for (var i=0; i < data.data.length; i++) {
-        var tmp =  data.data[i].charCodeAt(0).toString(2);
-        for(var j=0; j <= 8-tmp.length; j++){
-          tmp = "0" + tmp;
+      if(!obj.busy){
+        obj.busy = true;
+        var data = url.parse(req.url, true).query;
+        var binary_data="";
+        for (var i=0; i < data.data.length; i++) {
+          var tmp =  data.data[i].charCodeAt(0).toString(2);
+          for(var j=0; j <= 8-tmp.length; j++){
+            tmp = "0" + tmp;
+          }
+          binary_data += tmp;
         }
-        binary_data += tmp;
+        fs.writeFile('file.txt', data.data, function(err){
+          if(err) throw err;
+          console.log(data.data);
+        })
+        res.end('recieved data!');
       }
-      fs.writeFile(`./${data.row}.txt`, binary_data, function(err){
-        if(err) throw err;
-        console.log('finished writing to file');
-      })
-      res.end('recieved data!');
+      else{
+        res.writeHeader(400, {'Content-type' : 'application/json'});
+        res.write(JSON.stringify(obj.busy));
+        res.end();
+      }
+
     break;
 
     default:
@@ -41,5 +63,5 @@ var httpServer = http.createServer(function(req, res){
   res.end();
 
 }).listen(port, function(){
-  console.log('server running on port 8080...');
+  // console.log('server running on port 8080...');
 });
